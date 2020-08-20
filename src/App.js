4a255e39f16dbb9287b8b936e73c94bbc13d3954
doreen162 <60643196+Doreen162@users.noreply.github.com>
2020-08-20 10:26:57 +0200

    "project"

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Fragment, useEffect } from 'react';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/layout/Logs';
+import AddBtn from './components/logs/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/layout/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
+import { Provider } from 'react-redux';
+import store from './store';
+
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import './App.css';
+
+
+
+const App = () => {
+   useEffect(() => {
+    // Init Materialize JS
+    M.AutoInit();
+  });
+  return (
+    <Provider store={store}>
+    <Fragment>
+        <SearchBar />
+        <div className='container'>
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+      </Provider>
+  );
+};
+
+export default App;
---
+++ src/components/layout/PreLoader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PreLoader = () => {
+    return (
+        <div className="progress blue lighted-4">
+           <div className="interdeterminate blue"></div> 
+        </div>
+    )
+}
+
+export default PreLoader
